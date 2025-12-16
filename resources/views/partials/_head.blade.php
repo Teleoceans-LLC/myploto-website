bd47db6c6e80a2560b0ba917aebc32aa65cc77b1
Teleoceans Co <108214207+teleoceans@users.noreply.github.com>
2025-12-16 16:29:05 +0200

    .

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -40,8 +40,10 @@ src="https://www.facebook.com/tr?id=1006480110430765&ev=PageView&noscript=1"
 </script>
 
 <!-- Teleoceans Analytics -->
-<script
-    src="https://analytics.teleoceans-servers.cloud/api/script.js"
-    data-site-id="15"
-    defer
-></script>
+<!-- Privacy-friendly analytics by Plausible -->
+<script async src="https://analytics.teleoceans-servers.cloud/js/pa-cyUaigMDTpUprCktkOXxJ.js"></script>
+<script>
+  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
+  plausible.init()
+</script>
+
